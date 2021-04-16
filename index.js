912a2c0cab135f9b2790436a82c6e53b87447483
mnjoyanx <tigranmnjoyan@gmail.com>
2021-04-16 15:29:57 +0400

    cancel record

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let mediaRecorder;
-let recordedBlobs;
+let mediaRecorder
+let recordedBlobs
+let timer
+let accept = false
+
 
 const recordedVideo = document.querySelector('video#recorded');
 const recordButton = document.querySelector('button#record');
@@ -11,16 +14,28 @@ const actions = document.getElementById('action')
 const deleteBtn = document.getElementById('delete-btn')
 const recordMain = document.getElementById('record-main')
 
-recordButton.addEventListener('click', () => {
+const modalSelector = document.getElementById('modal')
+const errMessage = document.getElementById('accept-message')
+let inputSelector = document.getElementById('isChecked')
+const timerSelector = document.getElementById('timer-main')
+let first = document.getElementById('first')
+let second = document.getElementById('second')
+let minute = document.getElementById('minute')
+const fiveMinutes = document.getElementById('five-minutes')
+let notification = false
+const yourRecord = document.getElementById('your-record')
+const recordNotification = document.getElementById('record-notification')
+
+
+
+    recordButton.addEventListener('click', () => {
     if (recordButton.textContent === 'Record') {
         startRecording();
     } else {
-        stopRecording();
         recordButton.textContent = 'Record';
-        downloadButton.disabled = false;   
+        yourRecord.classList.remove('hidden')
     }
 });
-
 
 
 
@@ -36,7 +51,6 @@ downloadButton.addEventListener('click', () => {
     document.body.removeChild(a);
     window.URL.revokeObjectURL(url);
 
-    // }, 100);
 });
 
 function handleDataAvailable(event) {
@@ -47,19 +61,7 @@ function handleDataAvailable(event) {
     }
 }
 
-function startRecording() {
-    startMain.classList.add('hidden')
-    modal.classList.remove('hidden')
-    recordMain.classList.remove('hidden')
-    actions.classList.add('hidden')
-
-    if (recordedVideo.src != 'null') {
-        recordedVideo.src = null;
-
-        // recordedVideo.srcObject = null;
-        recordedVideo.classList.add('hidden')
-
-    }
+ function recordBlob() {
     recordedBlobs = [];
     let options = { mimeType: 'video/webm;codecs=vp9,opus' };
     try {
@@ -69,85 +71,184 @@ function startRecording() {
         return;
     }
 
-    console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+}
+
+function startRecording() {
+
+    timerSelector.classList.remove('hidden')
+    startMain.classList.add('hidden')
+    modal.classList.remove('hidden')
+    recordMain.classList.remove('hidden')
+    actions.classList.add('hidden')
+
+    if (recordedVideo.src != 'null') {
+        recordedVideo.classList.add('hidden')
+    }
+    recordBlob()
+
+
+    timerFn()
+
     recordButton.innerText = ''
     let stopSpan = document.createElement('span')
     stopSpan.classList.add('right-space')
     stopSpan.textContent = 'Stop Recording'
     recordButton.append(stopSpan)
     const stopIcon = document.createElement('i')
-    
+
     stopIcon.classList.add('fa')
     stopIcon.classList.add('fa-stop-circle')
     recordButton.insertBefore(stopIcon, stopSpan)
-    downloadButton.disabled = true;
     
-    mediaRecorder.ondataavailable = handleDataAvailable;
-    mediaRecorder.start();
-    console.log('MediaRecorder started', mediaRecorder);
+    stopFn()
+    
 }
 
-async function stopRecording() {
 
-    mediaRecorder.onstop = (event) => {
-        console.log('Recorder stopped: ', event);
-        console.log('Recorded Blobs: ', recordedBlobs);
-    };
+async function stopFn(timer) {
+ 
+    clearInterval(timer)
+   
+    
+}
+
+recordButton.addEventListener('click', async () => {
     startMain.classList.remove('hidden')
     mediaRecorder.stop();
     actions.classList.remove('hidden')
     modal.classList.add('hidden')
 
-    setTimeout(() => {
+
+    
+    // videoSelector.srcObject = null
+    // videoSelector.classList.add('hidden')
+    // startRecordingSelector.classList.remove('hidden')
+    // stopRecordingSelector.classList.add('hidden')
+
+
+    setTimeout(async () => {
         recordedVideo.classList.remove('hidden')
         const superBuffer = new Blob(recordedBlobs, { type: 'video/webm' });
-        recordedVideo.src = null;
-        recordedVideo.srcObject = null;
+        // recordedVideo.src = null;
+        // recordedVideo.srcObject = null;
         recordedVideo.src = window.URL.createObjectURL(superBuffer);
         recordedVideo.controls = true;
         recordedVideo.play();
+    // mediaRecorder.stream.getVideoTracks()[0].addEventListener('ended', () => alert(88888888))
+        stopTrack()
+
     }, 100)
 
-}
+
+})
+
 
 function handleSuccess(stream) {
-    recordButton.disabled = false;
     console.log('getUserMedia() got stream:', stream);
     window.stream = stream;
 
+      stream.getVideoTracks()[0].onended = function () {
+        //   clearInterval(timer)
+        //     timerSelector.classList.add('hidden')
+          recordButton.click()
+    };
     startRecording()
-        // const gumVideo = document.querySelector('video#gum');
-        // gumVideo.srcObject = stream;
 }
 
 async function init(constraints) {
+        const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
 
     try {
-        const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+        console.log(stream, 8999999999999)
+        
         handleSuccess(stream);
+
     } catch (e) {
+        console.log(stream, 8999999999999)
+
         console.error('navigator.getUserMedia error:', e);
+        return
     }
 }
 
-document.querySelector('button#start').addEventListener('click', async() => {
+document.querySelector('button#start').addEventListener('click', async () => {
+    modalSelector.classList.remove('hidden')
+
+    inputSelector.checked = false
     const constraints = {
         video: {
             width: 500,
             height: 400
         }
     };
-    await init(constraints);
+
+
+    let startRecording = document.getElementById('start-recording')
+    
+    startRecording.addEventListener('click', async () => {
+        if (inputSelector.checked) {
+            modalSelector.classList.add('hidden')
+            await init(constraints);
+        } else {
+            errMessage.classList.remove('hidden')
+        }
+    })
 });
 
 deleteBtn.addEventListener('click', () => {
-    // recordedVideo.classList.add('hidden')
-    // const superBuffer = new Blob(recordedBlobs, { type: 'video/webm' });
+    recordedVideo.classList.add('hidden')
+    const superBuffer = new Blob(recordedBlobs, { type: 'video/webm' });
     recordedVideo.src = null;
     recordedVideo.srcObject = null;
     recordMain.classList.add('hidden')
     actions.classList.add('hidden')
-        // recordedVideo.src = window.URL.createObjectURL(superBuffer);
-        // recordedVideo.controls = true;
-        // recordedVideo.play();
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+    recordedVideo.controls = true;
+    yourRecord.classList.add('hidden')
+    // recordedVideo.play();
 })
+
+
+const b = arr => {
+    return `${arr[0]}:${arr[1]}${arr[2]}`
+}
+
+function timerFn() {
+    let _t = [0, 0, 0]
+
+    timer = setInterval(async () => {
+        const time = +_t.join('')
+        fiveMinutes.classList.remove('hidden')
+        if (time >= 500) {
+            stopFn(timer)
+            _t = [0, 0, 0]
+            
+            stopTrack()
+            recordButton.click()
+            recordNotification.classList.remove('hidden')
+            return 
+        }
+        _t[2]++
+
+        if (_t[2] === 10) {
+            _t[2] = 0
+            _t[1]++
+        }
+
+        if (_t[1] === 6) {
+            _t[1] = 0
+            _t[0]++
+        }
+        
+        minute.textContent = b(_t)
+    }, 1000)
+
+}
+
+
+async function stopTrack() {
+    let b = await mediaRecorder.stream.getVideoTracks()[0]
+    b.stop()
+}
